0f2d61f86463b31f0db01bd0f7c5235eb4735bdb
Wojciech Skarbek <wojciech.skarbek@kainos.com>
2022-07-14 15:18:45 +0200

    added: - integration tests for createUser changed: - createUser path to /user/register

--- src/main/java/org/kainos/groupA/controller/UserController.java
+++ src/main/java/org/kainos/groupA/controller/UserController.java
@@ -30,7 +30,7 @@ public class UserController {
     }
 
     @POST
-    @Path("/users")
+    @Path("/user/register")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response createUser(User user) {
@@ -38,7 +38,6 @@ public class UserController {
             try {
                 return Response.ok(userService.createUser(user)).build();
             } catch (SQLException | NullPointerException e) {
-                System.out.println("SIEMA " + e);
                 return Response.status(HttpStatus.INTERNAL_SERVER_ERROR_500).entity(e.getMessage()).build();
             }
         } else {
---
+++ src/test/java/org/kainos/groupA/integration/UserIntegrationTest.java
@@ -0,0 +1,44 @@
+package org.kainos.groupA.integration;
+
+import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.kainos.groupA.GroupAApplication;
+import org.kainos.groupA.GroupAConfiguration;
+import org.kainos.groupA.models.User;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.hibernate.validator.internal.util.Contracts.assertTrue;
+
+@ExtendWith(DropwizardExtensionsSupport.class)
+public class UserIntegrationTest {
+    User testUser =  new User(
+            "test@email.com",
+            "encryptedpass",
+            User.Role.Employee,
+            "token",
+            "Joe",
+            "Doe",
+            "123456789",
+            User.Location.Birmingham
+    );
+    static final DropwizardAppExtension<GroupAConfiguration> APP = new DropwizardAppExtension<GroupAConfiguration>(
+            GroupAApplication.class, null,
+            new ResourceConfigurationSourceProvider()
+    );
+
+    @Test
+    void createUser_shouldReturnCreatedUserId() {
+        int response = APP.client().target("http://localhost:8080/api/user/register")
+                .request()
+                .post(Entity.entity(testUser, MediaType.APPLICATION_JSON))
+                .readEntity(Integer.class);
+        Assertions.assertTrue(response > 0);
+    }
+}
