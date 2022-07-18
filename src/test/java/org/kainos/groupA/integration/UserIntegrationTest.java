ce8eebcb2302a79f8733048b58d602131413a2f2
Wojciech Skarbek <wojciech.skarbek@kainos.com>
2022-07-18 12:24:43 +0200

    changed: - fixed create user integration test

--- src/test/java/org/kainos/groupA/integration/UserIntegrationTest.java
+++ src/test/java/org/kainos/groupA/integration/UserIntegrationTest.java
@@ -3,6 +3,7 @@ package org.kainos.groupA.integration;
 import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+import org.eclipse.jetty.http.HttpStatus;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,12 +15,15 @@ import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import java.nio.charset.Charset;
+import java.util.Random;
+
 import static org.hibernate.validator.internal.util.Contracts.assertTrue;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class UserIntegrationTest {
     User testUser =  new User(
-            "test@email.com",
+            "test@kainos.com",
             "encryptedpass",
             User.Role.Employee,
             "token",
@@ -33,12 +37,21 @@ public class UserIntegrationTest {
             new ResourceConfigurationSourceProvider()
     );
 
+    private String generateEmail() {
+        byte[] array = new byte[7];
+        new Random().nextBytes(array);
+        String randomString = new String(array, Charset.forName("UTF-8"));
+        return randomString + "@kainos.com";
+    }
+
     @Test
-    void createUser_shouldReturnCreatedUserId() {
-        Response response = APP.client().target("https://localhost:8080/api/user/register")
+    void createUser_shouldReturnCreatedUserId()
+    {
+        testUser.setEmail(generateEmail());
+        Response response = APP.client().target("http://localhost:8080/api/user/register")
                 .request()
-                .post(Entity.entity(testUser, MediaType.APPLICATION_JSON))
-                .readEntity(Response.class);
-        Assertions.assertTrue(response > 0);
+                .post(Entity.entity(testUser, MediaType.APPLICATION_JSON));
+        Assertions.assertEquals(response.getStatus(), HttpStatus.OK_200);
+        Assertions.assertTrue(response.readEntity(Integer.class) > 0);
     }
 }
