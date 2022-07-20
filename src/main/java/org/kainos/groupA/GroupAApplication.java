2f740a1ef5fb87c4cdb15a7da6d6f82b09f02f9d
Wojciech Skarbek <wojciech.skarbek@kainos.com>
2022-07-20 12:55:35 +0200

    added: - login endpoint now returns JWTToken on successful login

--- src/main/java/org/kainos/groupA/GroupAApplication.java
+++ src/main/java/org/kainos/groupA/GroupAApplication.java
@@ -1,13 +1,22 @@
 package org.kainos.groupA;
 
+import com.github.toastshaman.dropwizard.auth.jwt.JwtAuthFilter;
+import io.dropwizard.auth.AuthDynamicFeature;
+import io.dropwizard.auth.AuthValueFactoryProvider;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.federecio.dropwizard.swagger.SwaggerBundle;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
-import org.kainos.groupA.controller.JobResponsibilityController;
-import org.kainos.groupA.controller.JobRoleController;
-import org.kainos.groupA.controller.UserController;
-import org.kainos.groupA.controller.BandCompController;
+import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
+import org.jose4j.keys.HmacKey;
+import org.kainos.groupA.controller.*;
+import org.kainos.groupA.dao.UserDao;
+import org.kainos.groupA.models.LoginUser;
+
+import java.io.UnsupportedEncodingException;
+import java.security.Principal;
 
 /**
  * Main class of the application.
@@ -38,10 +47,32 @@ public class GroupAApplication extends io.dropwizard.Application<GroupAConfigura
      */
     @Override
     public void run(final GroupAConfiguration configuration,
-                    final Environment environment) {
-        environment.jersey().register(new UserController());
+                    final Environment environment) throws UnsupportedEncodingException {
+        final byte[] key = configuration.getJwtTokenSecret();
+
+        final JwtConsumer consumer = new JwtConsumerBuilder()
+                .setAllowedClockSkewInSeconds(30) // allow some leeway in validating time based claims to account for clock skew
+                .setRequireExpirationTime() // the JWT must have an expiration time
+                .setRequireSubject() // the JWT must have a subject claim
+                .setVerificationKey(new HmacKey(key)) // verify the signature with the public key
+                .setRelaxVerificationKeyValidation() // relaxes key length requirement
+                .build(); // create the JwtConsumer instance
+
+        environment.jersey().register(new AuthDynamicFeature(
+                new JwtAuthFilter.Builder<LoginUser>()
+                        .setJwtConsumer(consumer)
+                        .setRealm("realm")
+                        .setPrefix("Bearer")
+                        .setAuthenticator(new UserDao(configuration.getJwtTokenSecret()))
+                        .buildAuthFilter()));
+
+        //Controllers
+        environment.jersey().register(new UserController(configuration.getJwtTokenSecret()));
         environment.jersey().register(new JobRoleController());
         environment.jersey().register(new BandCompController());
         environment.jersey().register(new JobResponsibilityController());
+        //Authentication
+        environment.jersey().register(new AuthValueFactoryProvider.Binder<>(Principal.class));
+        environment.jersey().register(RolesAllowedDynamicFeature.class);
     }
 }
